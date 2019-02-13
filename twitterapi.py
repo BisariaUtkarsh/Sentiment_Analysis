4e9a37fff6d21a357f6fd33558606f95035b76d8
Utkarsh Bisaria <2utkarsh_bt2k16@dtu.ac.in>
2019-02-14 04:06:56 +0530

    Delete twitterapi.py

--- twitterapi.py
+++
@@ -1,8 +0,0 @@
-
-ckey = "c2Hcccz3veVjH9FbY6UPgBM6X"  # (API key)
-
-csecret = "wVMbASCiRjfi6hi59tbXYBqUITROvBQwdPuzV1r40JmCm8E11K"   # (API secret key)
-
-atoken = "151932822-hUmm1sxgIVVrzW99ydO9eAWYDQpRGRux6Wrp5Ja9"    # (Access token)
-
-asecret = "LpTGShHQlylaA3R4yzWGfFdkr372PSSb6eICKFfnXDUZv"     # (Access token secret)
